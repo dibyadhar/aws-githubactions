c13e960021976b02ecd8036aba9aabd3ca690100
Dibya Dhar <dibya.dhar@amido.com>
2022-03-23 12:02:35 +0000

    Update terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
-    }
-  }
-
-  required_version = ">=0.14.9"
-
  backend "s3" {
     bucket = "amido-stacks-backend"
     key    = "compute"
